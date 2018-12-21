17027526446b293b460d23ae8d63551c42bac128
Kozlowski Krzysztof <krzysztof.1.kozlowski@nokia.com>
2018-12-21 22:46:24 +0100

    better version

--- src/WiMD.Hub/LocationHub.cs
+++ src/WiMD.Hub/LocationHub.cs
@@ -13,9 +13,9 @@ namespace WiMD.Hub
             //Clients.All.SendAsync("broadcastMessage", Context.User.Identity.Name, "", "");
             return base.OnConnectedAsync();
         }
-        public void Send(string token, decimal longitute, decimal latitude)
+        public void Send(string token, Location location)
         {
-            Clients.All.SendAsync("broadcastMessage", Context.User.Identity.Name, longitute, latitude);
+            Clients.All.SendAsync("broadcastMessage", token, location);
         }
 
         public override Task OnDisconnectedAsync(Exception exception)
@@ -23,5 +23,11 @@ namespace WiMD.Hub
             //Clients.All.SendAsync("broadcastMessage", Context.User.Identity.Name, 0, 0);
             return base.OnDisconnectedAsync(exception);
         }
+    }
+
+    public class Location
+    {
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
     }
 }
