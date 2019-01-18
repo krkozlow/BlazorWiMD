0eab38166b7d4d58770810ac01927d9bb600b8e9
Kozlowski Krzysztof <krzysztof.1.kozlowski@nokia.com>
2019-01-18 13:57:17 +0100

    [FE] extract SignIn to component

---
+++ src/WiMD.Client/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WiMD.Client.Models
+{
+    public class User
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ src/WiMD.Client/api/BaseApi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace WiMD.Client.api
+{
+    public static class BaseApi
+    {
+        const string baseUrl = "http://localhost:64827/api/";
+
+        public static Uri BaseUrl => new Uri(baseUrl);
+        public static string SignInUrl => new Uri(BaseUrl, "Account/SignIn").ToString();
+        public static string LogInUrl => new Uri(BaseUrl, "Account/LogIn").ToString();
+    }
+}
